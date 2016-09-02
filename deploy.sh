f3bc2e0c4e47345751e2e9721a3d6b97567a576b
211016759 <wes.bornor@ge.com>
2016-09-02 00:18:40 -0400

    buildId to trigger CFN update

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,16 @@ export AWS_CLI_PROFILE="freshpad";
 export CFN_JSON="cfn.json";
 export CFN_PARAM_JSON="cfn-param-$ENV.json";
 export STACK_NAME="freshpad-lambda-askfreshpad-$ENV";
+export NOW=`date +%y%m%d%H%M%S`;
 
 npm install
 
 zip -r ./$SKILL_NAME.zip index.js LICENSE README.md package.json src node_modules config
 
 aws s3 cp --profile $AWS_CLI_PROFILE \
-./$SKILL_NAME.zip s3://$SKILL_NAME/artifacts/lambda/$LAMBDA_NAME/$ENV.zip;
+./$SKILL_NAME.zip s3://$SKILL_NAME/artifacts/lambda/$LAMBDA_NAME/$ENV-$NOW.zip;
+
+sed -i -e s/\${BuildId}/$NOW/g $CFN_PARAM_JSON
 
 aws cloudformation validate-template --profile $AWS_CLI_PROFILE --template-body file://$CFN_JSON
 
